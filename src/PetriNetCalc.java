f4dcdb130e36a6bc195f6c5a8a63cce213f71a98
JWahlers <JWahlers15@gmail.com>
2015-12-02 20:56:36 -0500

    Tidying Up
    Code made neater and all basic functions working as intended.
    runMarking() and uniqueness and boolean array still need implementing

--- src/PetriNetCalc.java
+++ src/PetriNetCalc.java
@@ -1,186 +1,78 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.regex.*;
 
 public class PetriNetCalc
 {
+    //Regex definitions
+    static final String POS_INT = "\\d+";
+    static final String STATE =  "[(]?(\\d+, )*(\\d+)+[)]?";
+
+
+
+
+
+    //Variable declarations
+    static Scanner in = new Scanner(System.in);
+    static String input;
+    static boolean safeInput=false;
+    static int noPlaces=0;
+    static int noTrans=0;
+    static int transIn[][];
+    static int transOut[][];
+    static String delim = " ";
+    static ArrayList<String> markings = new ArrayList<String>();
+
+
+
+
+
     public static void main(String[] args)
     {
-        //Regex definitions
-        final String POS_INT = "\\d+";
-        final String STATE =  "[(]?(\\d+, )*(\\d+)+[)]?";
-
-
-
-
-
-        //Variable declarations
-        Scanner in = new Scanner(System.in);
-        String input;
-        boolean safeInput=false;
-        int noPlaces=0;
-        int noTrans=0;
-        int transIn[][];
-        int transOut[][];
-        String delim = " ";
-
-
-
-
-        
         //Process number of Places
-        do
-        {
-            System.out.println("Please enter the number of Places in your net:  ");
-            input = in.nextLine();
-            if(verify(input, POS_INT))
-                safeInput = true;
-            else
-                System.out.println("\n\n\nERROR - Please enter a positive integer!" +
-                                       "\n----------------------------------------\n\n\n");
-        }while(!safeInput);
-
-
-
-
-
-        //Create Places
-        noPlaces = Integer.parseInt(input);
-        System.out.println("\n\n\nYou entered:  " + noPlaces + "\n\n\n");
-
-
+        getNumPlaces();
 
 
 
         //Process number of Transitions
-        safeInput = false;
-        do
-        {
-            System.out.println("Please enter the number of Transitions in your net:  ");
-            input = in.nextLine();
-            if(verify(input, POS_INT))
-                safeInput = true;
-            else
-                System.out.println("\n\n\nERROR - Please enter a positive integer!" +
-                        "\n----------------------------------------\n\n\n");
-        }while(!safeInput);
-
-
-
-
-
-        //Create Transitions
-        noTrans = Integer.parseInt(input);
-        System.out.println("\n\n\n You entered:  " + noTrans + "\n\n\n");
+        getNumTrans();
 
         
-        
-        
-        
+
         //Create trans Arrays
         transIn = new int[noTrans][noPlaces];
         transOut = new int[noTrans][noPlaces];
 
 
-
-
         
         //Process Transition Inputs
-       for(int i=0; i < noTrans; i++)
-       {
-            safeInput = false;
-            do
+        processTransIn();
+
+
+            
+        //Process Transition Outputs
+        processTransOut();
+            
+            
+            
+        //DEBUG - this block is for debugging only. Makes sure that the values stored inside transIn
+        //and transOut are making sense
+        for(int i=0; i<noTrans; i++)
+            for(int j=0; j<noPlaces; j++)
             {
-                System.out.println("Please enter the inputs for Transition " + (i+1) +":  ");
-                input = in.nextLine();
-                if(verify(input, noPlaces))
-                {
-                    safeInput = true;
+                System.out.print("[" + transIn[i][j] + "] ");
+                if(j == noPlaces - 1)
                     System.out.println();
-                    input = input.replace("(","");
-                    input = input.replace(",", "");
-                    input = input.replace(")", "");
-                    
-                    
-
-                    String[] s;
-                    s = input.split(delim);
-                    
-                    
-                    
-                    for(int j=0; j<noPlaces; j++)
-                    {
-                    	System.out.println(s[j]);
-                    	transIn[i][j] = Integer.parseInt(s[j]);
-                    }
-                    
-                    
-                    
-                }
-                else
-                System.out.println("\n\n\nERROR - Please enter your inputs in the format (int, int, . . . int)" +
-                                       "\n--------------------------------------------------------------------\n\n\n");
-            }while(!safeInput);
-       }
-            
-       
-            
-            
-            
-            //Process Transition Outputs
-            for(int i=0; i < noTrans; i++)
-            {
-                 safeInput = false;
-                 do
-                 {
-                     System.out.println("Please enter the outputs for Transition " + (i+1) +":  ");
-                     input = in.nextLine();
-                     if(verify(input, noPlaces))
-                     {
-                         safeInput = true;
-                         System.out.println();
-                         input = input.replace("(","");
-                         input = input.replace(",", "");
-                         input = input.replace(")", "");
-                         
-                         
-
-                         String[] s;
-                         s = input.split(delim);
-                         
-                         
-                         
-                         for(int j=0; j<noPlaces; j++)
-                         {
-                        	 System.out.println(s[j]);
-                        	 transOut[i][j] = Integer.parseInt(s[j]);
-                         }
-                     }
-                     else
-                     System.out.println("\n\n\nERROR - Please enter your outputs in the format (int, int, . . . int)" +
-                                            "\n--------------------------------------------------------------------\n\n\n");
-                 }while(!safeInput);
             }
-            
-            
-            
-            
-            
-            //DEBUG - this block is for debugging only. Makes sure that the values stored inside transIn 
-            //and transOut are making sense
-            for(int i=0; i<noTrans; i++)
-            	for(int j=0; j<noPlaces; j++)
-            	{
-            		System.out.print("[" + transIn[i][j] + "] ");
-            		if(j == noPlaces - 1)
-            			System.out.println();
-            	}
-            
-            
-            
-            
-            
-            in.close();
-       }
+
+
+
+        getInit();
+
+
+
+        in.close();
+    }
     
 
 
@@ -214,5 +106,199 @@ public class PetriNetCalc
            return true;
         else
            return false;
+    }
+
+
+
+
+
+    //Method asks for and stores initial Marking
+    private static void getInit()
+    {
+        safeInput = false;
+        do
+        {
+            System.out.println("Please enter the initial marking for your Net:  ");
+            input = in.nextLine();
+            if(verify(input, noPlaces))
+            {
+                safeInput = true;
+                System.out.println();
+                input = input.replace("(","");
+                input = input.replace(",", "");
+                input = input.replace(")", "");
+
+                markings.add(input);
+            }
+            else
+                System.out.println("\n\n\nERROR - Please enter your marking in the format (int, int, . . . int)" +
+                                       "\n--------------------------------------------------------------------\n\n\n");
+        }while(!safeInput);
+    }
+
+
+
+
+
+    //Method converts markings into an array of integers for usability
+    private static int[] convertMarking(String in)
+    {
+        String[] s;
+        s = in.split(delim);
+        int[] temp = new int[noPlaces];
+        for(int i=0; i<noPlaces; i++)
+        {
+            temp[i] = Integer.parseInt(s[i]);
+        }
+
+        return temp;
+    }
+
+
+
+
+
+    //Method finds runs through the ArrayList markings and uses the markings within it to create a new markings until no
+    //unique markings are generated.
+    private static void runMarkings()
+    {
+
+    }
+
+
+
+
+
+    //Process Transout
+    private static void processTransOut()
+    {
+        for(int i=0; i < noTrans; i++)
+        {
+            safeInput = false;
+            do
+            {
+                System.out.println("Please enter the outputs for Transition " + (i+1) +":  ");
+                input = in.nextLine();
+                if(verify(input, noPlaces))
+                {
+                    safeInput = true;
+                    System.out.println();
+                    input = input.replace("(","");
+                    input = input.replace(",", "");
+                    input = input.replace(")", "");
+
+
+
+                    String[] s;
+                    s = input.split(delim);
+
+
+
+                    for(int j=0; j<noPlaces; j++)
+                    {
+                        System.out.println(s[j]);
+                        transOut[i][j] = Integer.parseInt(s[j]);
+                    }
+                }
+                else
+                    System.out.println("\n\n\nERROR - Please enter your outputs in the format (int, int, . . . int)" +
+                            "\n--------------------------------------------------------------------\n\n\n");
+            }while(!safeInput);
+        }
+    }
+
+
+
+
+
+    //Get Number of Places
+    private static void getNumPlaces()
+    {
+        do
+        {
+            System.out.println("Please enter the number of Places in your net:  ");
+            input = in.nextLine();
+            if(verify(input, POS_INT))
+                safeInput = true;
+            else
+                System.out.println("\n\n\nERROR - Please enter a positive integer!" +
+                        "\n----------------------------------------\n\n\n");
+        }while(!safeInput);
+
+        //Create Places
+        noPlaces = Integer.parseInt(input);
+        System.out.println("\n\n\nYou entered:  " + noPlaces + "\n\n\n");
+    }
+
+
+
+
+
+    //Get number of trans
+    private static void getNumTrans()
+    {
+        safeInput = false;
+        do
+        {
+            System.out.println("Please enter the number of Transitions in your net:  ");
+            input = in.nextLine();
+            if(verify(input, POS_INT))
+                safeInput = true;
+            else
+                System.out.println("\n\n\nERROR - Please enter a positive integer!" +
+                        "\n----------------------------------------\n\n\n");
+        }while(!safeInput);
+
+
+
+        //Create Transitions
+        noTrans = Integer.parseInt(input);
+        System.out.println("\n\n\n You entered:  " + noTrans + "\n\n\n");
+    }
+
+
+
+
+
+
+    //Process transIn
+    private static void processTransIn()
+    {
+        for(int i=0; i < noTrans; i++)
+        {
+            safeInput = false;
+            do
+            {
+                System.out.println("Please enter the inputs for Transition " + (i+1) +":  ");
+                input = in.nextLine();
+                if(verify(input, noPlaces))
+                {
+                    safeInput = true;
+                    System.out.println();
+                    input = input.replace("(","");
+                    input = input.replace(",", "");
+                    input = input.replace(")", "");
+
+
+
+                    String[] s;
+                    s = input.split(delim);
+
+
+
+                    for(int j=0; j<noPlaces; j++)
+                    {
+                        System.out.println(s[j]);
+                        transIn[i][j] = Integer.parseInt(s[j]);
+                    }
+
+
+
+                }
+                else
+                    System.out.println("\n\n\nERROR - Please enter your inputs in the format (int, int, . . . int)" +
+                            "\n--------------------------------------------------------------------\n\n\n");
+            }while(!safeInput);
+        }
     }
 }
